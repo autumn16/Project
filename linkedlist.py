1628b4f9ebf15c67d4d200a190de8da52c4abf5c
minhtri <minhtri.luu10@gmail.com>
2020-03-18 14:16:19 +0700

    Implementing linkedlist using python

---
+++ linkedlist.py
@@ -0,0 +1,57 @@
+# 18 3
+
+class node:
+    def __init__(self,data=None):
+        self.data = data
+        self.next = None
+
+class LList:
+    def __init__(self):
+        self.head = None
+        self.rsize = 0
+
+    # this function is to check whether the list is empty or not
+    def isEmpty(self):
+        if(self.rsize==0): return True
+        else: return False
+    
+    # this function is to pushback element
+    def pushBack(self,value):
+        if(self.isEmpty()==True):
+            self.head = node(value)
+            self.rsize+=1
+            print(self.head.data,"is now head")
+            self.ptr = self.head
+        else : 
+            self.ptr.next = node(value)
+            self.ptr = self.ptr.next
+            self.ptr.next = None
+            print(self.ptr.data,"pushed successfully into list")
+
+    # this function is to travel the list and print out value
+    def print(self):
+        self.ptr = self.head
+        while(self.ptr!=None):
+            print("This element's value is",self.ptr.data)
+            self.ptr = self.ptr.next
+
+    def pushHead(self,value):
+        self.ptr = node(value)
+        self.ptr.next = self.head
+        self.head = self.ptr
+        print(self.head.data,"is now head")
+
+
+x = LList()
+x.pushBack(1)
+x.pushBack(2)
+x.pushBack(3)
+x.pushHead(5)
+x.print()
+
+
+
+
+
+
+
---
+++ travel.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+struct toRead{
+    int pos1;
+    int pos2;
+    int numcost;
+}
+
+int main(){
+    for
+    return 0;
+}
