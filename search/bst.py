b55733f02c41f7ae76a63fa05443ef6a9e6c1d67
minhtri <minhtri.luu10@gmail.com>
2020-03-28 21:20:25 +0700

    bst python

--- search/bst.py
+++ search/bst.py
@@ -1,16 +1,32 @@
+# 2020 3 28
+# Binary search tree implementation using Python
+
 class node:
     def __init__(self,value):
         self.left = None
         self.right = None
         self.data = value
 
-class bst:
+
+class binaryTree:
     def __init__(self):
         self.rsize = 0
-        self.node.root = None
+        self.root = node(None)
+
     def isEmpty(self):
         if(self.rsize==0): return True
         else: return False
 
+    def insert(self,root, value):
+        if(self.root==None):
+            self.root = node(value)
+            print(self.root.data,"successfully pushed into tree")
+        else:
+            if(value<self.root.data): self.insert(self.root.left,value)
+            if(value>self.root.data): self.insert(self.root.right,value)
 
-    
+
+myt = binaryTree()
+r = node(100)
+myt.insert(r,4)
+myt.insert(r,2)
