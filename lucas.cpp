223ea9e7a617ad2fcdc0354024b1dab589f8dca4
minhtri <minhtri.luu10@gmail.com>
2020-03-30 21:20:27 +0700

    snail problem

--- lucas.cpp
+++ lucas.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
 using namespace std;
+#define MAX 101
+int n,m,y,x;
+int arr[MAX][MAX];
+int count = 0;
+
+void solve(){
+
+}
 
 int main(){
-    string s;
-    getline(cin,s);
-    cout << s << endl;
+    cin >> n >> m >> y >> x;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
