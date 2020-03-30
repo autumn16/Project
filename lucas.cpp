aeaf663c5733ab5bee84556e5a8b48f6d0f51f0c
Minhtri <46084113+mitapari@users.noreply.github.com>
2020-03-30 12:17:02 +0700

    test wsl on windows

--- lucas.cpp
+++ lucas.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
+
 int main(){
-    vector<int> myVec;
-    vector<int>::iterator ptr;
-    myVec.push_back(1);
-    myVec.push_back(2);
-    myVec.push_back(3);
-    ptr = myVec.begin();
-    auto pTemp = myVec.begin();
-    //myVec.erase(ptr);
-    //myVec.erase(ptr);
-    for(int i=0; i<myVec.size(); i++){
-        cout << myVec[i] << " ";
-    }
-    cout << endl;
+    string s;
+    getline(cin,s);
+    cout << s << endl;
     return 0;
 }
